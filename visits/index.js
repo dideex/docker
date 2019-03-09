8bfe426e77b8a91b2f3131ea12bc4d6da6e3612b
DiDex <wc34fun@mail.ru>
2019-03-09 14:15:51 +0800

    docker-compose

--- visits/index.js
+++ visits/index.js
@@ -2,10 +2,14 @@ const express = require('express')
 const redis = require('redis')
 
 const app = express()
-const client = redis.createClient()
+const client = redis.createClient({
+  host: 'redis-server',
+  port: 6379
+})
 client.set('visits', 0)
 
 app.get('/', (req, res) => {
+  process.exit(1)
   client.get('visits', (err, visits) => {
     res.send(`Number of visits is ${visits}`)
     client.set('visits', +visits + 1)
